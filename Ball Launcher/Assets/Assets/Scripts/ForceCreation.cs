1ab7abf745d946509b21c6500f651a45cc6ef636
Cody <webb.cody@hotmail.com>
2017-09-16 00:50:44 -0400

    Setting up project push

---
+++ Ball Launcher/Assets/Assets/Scripts/ForceCreation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceCreation : MonoBehaviour
+{
+
+    //public Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("space") /*&& this.GetComponent<Rigidbody>().useGravity == true*/)
+        {
+            this.GetComponent<Rigidbody>().useGravity = false;
+            print("space detected");
+        }
+        else
+        {
+            this.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
